a4d1ec0731871628e3d26d9960e550cf1e0074ce
tiagosathler <sathler@gmail.com>
2021-06-26 20:07:03 -0300

    Programa 1: done

---
+++ fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-1b-javascript-primeiros-passos/script.js
@@ -0,0 +1,41 @@
+// Bloco 04 - Dia 1 - Parte B
+// JavaScript - Primeiros Passos
+// 23/06/21 - Quarta-feira
+// Exercícios
+
+// p.1
+let a = 1;
+let b = 6;
+let resultado = 0 ;
+
+console.log('Dados a = ' + a + ' e b = ' + b)
+
+// p. 1.1
+console.log('Programa 1.1 - Adição')
+resultado = a + b;
+console.log("a + b = " + resultado);
+console.log('-----------')
+
+// p. 1.2
+console.log('Programa 1.2 - Subtração')
+resultado = a - b;
+console.log("a - b = " + resultado);
+console.log('-----------')
+
+// p. 1.3
+console.log('Programa 1.3 - Multiplicação')
+resultado = a * b;
+console.log("a * b = " + resultado);
+console.log('-----------')
+
+// p. 1.4
+console.log('Programa 1.4 - Divisão')
+resultado = a / b;
+console.log("a / b = " + resultado);
+console.log('-----------')
+
+// p. 1.5
+console.log('Programa 1.5 - Resto da divisão')
+resultado = a % b;
+console.log("a % b = " + resultado);
+console.log('-----------')

5c7d018dfe494db32645dd4fecaf410dd70aacf4
tiagosathler <sathler@gmail.com>
2021-06-26 18:51:54 -0300

    Bônus 1: done

--- fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-2-javascript-array-e-loop-for/script.js
@@ -88,3 +88,17 @@ for (let number of meusNumeros) {
   console.log(number/2)
 }
 console.log('-----------')
+
+// b.1
+console.log('Bloco 4 / Dia 2 / Bônus 1')
+for (let index = 1; index < numbers.length; index += 1) {
+  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+    if (numbers[index] < numbers[secondIndex]) {
+      let position = numbers[index];
+      numbers[index] = numbers[secondIndex];
+      numbers[secondIndex] = position;
+    }
+  }
+}
+console.log(numbers)
+console.log('-----------')

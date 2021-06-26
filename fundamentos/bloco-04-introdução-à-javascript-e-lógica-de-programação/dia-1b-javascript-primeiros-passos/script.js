4bf9f92032f2c861c93c348bc41dfa7a03412acb
tiagosathler <sathler@gmail.com>
2021-06-26 20:11:45 -0300

    Programa 3: done

--- fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-1b-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-1b-javascript-primeiros-passos/script.js
@@ -8,48 +8,63 @@ let a = 1;
 let b = 6;
 let resultado = 0 ;
 
-console.log('Dados a = ' + a + ' e b = ' + b)
+console.log('Dados a = ' + a + ' e b = ' + b);
 
 // p. 1.1
-console.log('Programa 1.1 - Adição')
+console.log('Programa 1.1 - Adição');
 resultado = a + b;
 console.log("a + b = " + resultado);
-console.log('-----------')
+console.log('-----------');
 
 // p. 1.2
-console.log('Programa 1.2 - Subtração')
+console.log('Programa 1.2 - Subtração');
 resultado = a - b;
 console.log("a - b = " + resultado);
-console.log('-----------')
+console.log('-----------');
 
 // p. 1.3
-console.log('Programa 1.3 - Multiplicação')
+console.log('Programa 1.3 - Multiplicação');
 resultado = a * b;
 console.log("a * b = " + resultado);
-console.log('-----------')
+console.log('-----------');
 
 // p. 1.4
-console.log('Programa 1.4 - Divisão')
+console.log('Programa 1.4 - Divisão');
 resultado = a / b;
 console.log("a / b = " + resultado);
-console.log('-----------')
+console.log('-----------');
 
 // p. 1.5
-console.log('Programa 1.5 - Resto da divisão')
+console.log('Programa 1.5 - Resto da divisão');
 resultado = a % b;
 console.log("a % b = " + resultado);
-console.log('-----------')
+console.log('-----------');
 
 // Programa 2
 a = 10;
 b = 9;
 console.log('Programa 2')
-console.log('Dados a = ' + a + ' e b = ' + b)
+console.log('Dados a = ' + a + ' e b = ' + b);
 if (a > b) {
-  console.log("a é maior que b = " + a + " > " + b)
+  console.log("a é maior que b = " + a + " > " + b);
   console.log("maior dos dois é... " + a)
 } else {
-  console.log("b é maior que a = " + b + " > " + a)
-  console.log("maior dos dois é... " + b)
+  console.log("b é maior que a = " + b + " > " + a);
+  console.log("maior dos dois é... " + b);
+}
+console.log('-----------')
+
+// Programa 3
+a = 15;
+b = 25;
+let c = 4;
+console.log('Programa 3');
+console.log('Dados a = ' + a + ', b = ' + b + ' e c = ' + c);
+if (a > b && a > c) {
+  console.log("maior dos três é... " + a);
+} else if (b > c) {
+  console.log("maior dos três é... " + b);  
+} else {
+  console.log("maior dos três é... " + c);
 }
 console.log('-----------')

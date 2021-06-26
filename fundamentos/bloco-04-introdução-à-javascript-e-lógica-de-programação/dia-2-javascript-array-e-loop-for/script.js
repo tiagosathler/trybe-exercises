9af3f67baa9526baaa6de16ffefeb577afa49976
tiagosathler <sathler@gmail.com>
2021-06-26 18:37:34 -0300

    Programa 1: done

---
+++ fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,12 @@
+// BLOCO 04 - DIA 2 - 24/06/21
+// JavaScript - Array e loop for
+// EXERCÍCIOS
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// P.1
+console.log('Bloco 4 / Dia 2 / Programa 1')
+for (let number of numbers) {
+  console.log(number);
+}
+console.log('-----------')

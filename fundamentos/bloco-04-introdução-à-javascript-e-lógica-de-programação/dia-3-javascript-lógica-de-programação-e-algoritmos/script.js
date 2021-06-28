e7ed41ca40c2161006cbbc0175da715cc236e490
tiagosathler <sathler@gmail.com>
2021-06-28 10:58:29 -0300

    Programa 1: done

--- fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-3-javascript-lógica-de-programação-e-algoritmos/script.js
+++ fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-3-javascript-lógica-de-programação-e-algoritmos/script.js
@@ -3,3 +3,20 @@
 // 25/06/21 - Tiago H. S. Sathler
 
 
+// P.1
+console.log('---------')
+console.log('Programa 1 - Quadro de n astericos')
+let n = 5;
+if (n<= 1) {
+  console.log('Valor de n deve ser maior que 1');
+} else {
+  let resultado = '*';
+  for (let index = 1; index < n ; index += 1) {
+    resultado += '*';
+  }
+  for (let index = 0; index < n ; index += 1) {
+    console.log(resultado);
+  }
+}
+console.log('---------')
+

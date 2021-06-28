1392d5fecfa922a60ead5451cd39904562bf5083
tiagosathler <sathler@gmail.com>
2021-06-28 20:22:25 -0300

    Programa I-1: done

--- fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-4-javascript-objetos-e-funções/script.js
+++ fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-4-javascript-objetos-e-funções/script.js
@@ -3,3 +3,20 @@
 // Exercícios do dia
 
 
+// Parte I - Objetos e For/In
+console.log('BLOCO 04 - DIA 4 - JAVASCRIPT - OBJETOS E FUNÇÕES');
+console.log('*****************************');
+console.log('PARTE I - Objetos e For / In');
+console.log('*****************************');
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+// programa I.1
+console.log('Programa I - 1');
+console.log('Boas-vindas para você, ' + info.personagem + '.');
+console.log('-----------------')
+

6c592535fd7d9d3331dffab07322129bf2a94c82
tiagosathler <sathler@gmail.com>
2021-06-28 21:41:46 -0300

    Programa II-4: done

--- fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-4-javascript-objetos-e-funções/script.js
+++ fundamentos/bloco-04-introdução-à-javascript-e-lógica-de-programação/dia-4-javascript-objetos-e-funções/script.js
@@ -100,7 +100,7 @@ function indexOfBigger(array) {
   return indexOfValue;
 }
 let arrayDeTeste = [2, 3, 6, 7, 10, 1];
-console.log("Dado o 'arrayDeTeste' = " + arrayDeTeste);
+console.log("Dado o 'arrayDeTeste' = ", arrayDeTeste);
 console.log("Índice do MAIOR valor no 'arrayDeTeste': " + indexOfBigger(arrayDeTeste));
 console.log('-----------------');
 
@@ -118,6 +118,25 @@ function indexOfSmaller(array) {
   return indexOfValue;
 }
 arrayDeTeste = [2, 4, 6, 7, 10, 0, -3];
-console.log("Dado o 'arrayDeTeste' = " + arrayDeTeste);
+console.log("Dado o 'arrayDeTeste' = ", arrayDeTeste);
 console.log("Índice do MENOR valor no 'arrayDeTeste': " + indexOfSmaller(arrayDeTeste));
 console.log('-----------------');
+
+// programa II.4
+console.log('Programa II - 4:');
+function biggestWord(array) {
+  let wordLength = 0;
+  let getWord = '';
+  for (let string of array) {
+    if (string.length > wordLength) {
+      wordLength = string.length;
+      getWord = string;
+    }
+  }
+  return getWord;
+}
+arrayDeTeste = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log("Dado o 'arrayDeTeste' = ", arrayDeTeste);
+console.log("Maior 'string' em 'arrayDeTeste': " + biggestWord(arrayDeTeste));
+console.log('-----------------');
+

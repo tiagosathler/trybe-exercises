891bbe0fcf16a20ab46881958d130a8d31c43d17
tiagosathler <sathler@gmail.com>
2021-07-25 15:30:32 -0300

    Parte II, exercício 5: feito

--- fundamentos/bloco-07-introdução-à-javascript-es6-e-testes-unitários/dia-2-javascript-es6-objects/script2.js
+++ fundamentos/bloco-07-introdução-à-javascript-es6-e-testes-unitários/dia-2-javascript-es6-objects/script2.js
@@ -37,3 +37,19 @@ console.log(showsLengthOfObject(lesson3));
 // EXERCÍCIO 4:
 const listValuesOfObject = object => Object.values(object);
 console.log(listValuesOfObject(lesson1));
+
+// EXERCÍCIO 5:
+// solução a:
+const allLessons1 = (object1, object2, object3) => {
+  const returnObject = { lesson1: {}, lesson2: {}, lesson3: {} };
+  Object.assign(returnObject.lesson1, object1);
+  Object.assign(returnObject.lesson2, object2);
+  Object.assign(returnObject.lesson3, object3);
+  return returnObject;
+}
+console.log(allLessons1(lesson1, lesson2, lesson3));
+
+// solução b:
+const allLessons2 = Object.assign({}, {lesson1, lesson2, lesson3});
+console.log(allLessons2);
+

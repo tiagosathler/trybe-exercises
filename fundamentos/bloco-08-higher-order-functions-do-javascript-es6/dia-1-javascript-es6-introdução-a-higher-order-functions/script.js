7b088b9600345edd508422aa1ac9e910bb91eb76
tiagosathler <sathler@gmail.com>
2021-07-28 20:33:36 -0300

    Exercícios pós-aula ao vivo - EXERCÍCIO 1: concluídos

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introdução-a-higher-order-functions/script.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introdução-a-higher-order-functions/script.js
@@ -1,3 +1,4 @@
+// BLOCO 08 - DIA 1 - JAVASCRIPT ES6 - INTRODUÇÃO A HIGHER ORDER FUNCTIONS
 // EXERCÍCIO PRÉ-AULA AO VIVO:
 
 const wakeUp = () => 'Acordando!!';
@@ -14,3 +15,25 @@ doingThings(coffee()); // Ela deve retornar o valor do respectivo parâmetro, ne
 doingThings(sleep()); // Ela deve retornar o valor do respectivo parâmetro, neste caso:
 // Partiu dormir!!
 
+
+// EXERCÍCIOS PÓS-AULA AO VIVO:
+
+// EXERCÍCIO 1:
+const makeObjectFunction = (nomeCompleto) => {
+  const emailAddress = `${nomeCompleto.replace(' ', '').toLowerCase()}@email.com`;
+  return { fullName: nomeCompleto, email: emailAddress};
+}
+
+const newEmployees = (funcaoDeRetorno) => {
+  const employees = {
+    id1: funcaoDeRetorno('Pedro Guerra'), 
+    // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: funcaoDeRetorno('Luiza Drumond'), 
+    // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: funcaoDeRetorno('Carla Paiva'), 
+    // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(makeObjectFunction));

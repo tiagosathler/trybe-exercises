a83a0beb0c861874aa3e1614cb48bb26ef3625a1
tiagosathler <sathler@gmail.com>
2021-07-29 15:23:16 -0300

    find Exercício 2: feito (generico)

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script-pre-aula.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script-pre-aula.js
@@ -23,3 +23,17 @@ const numbers = [19, 21, 30, 3, 45, 22, 15];
 const findDivisibleBy3And5 = (array) => array.find((element) => !(element % 3) && !(element % 5) );
 console.log(findDivisibleBy3And5(numbers))
 
+//2:
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+const isWordWithNLetters = (word, n) => word.length > n;
+const templateNegativeAnswer = (x) => `Não há nome com pelo menos ${x} letras`;
+
+const findFirstNameWithNLetters = (array, n) => {
+  const name = array.find((element) => isWordWithNLetters(element, n));
+  if (name !== undefined) {
+    return name;
+  }
+    return templateNegativeAnswer(n);
+}
+console.log(findFirstNameWithNLetters(names, 5));

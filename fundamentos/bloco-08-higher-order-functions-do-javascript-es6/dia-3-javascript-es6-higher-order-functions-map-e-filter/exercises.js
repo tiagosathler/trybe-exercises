3a91da04a55bae5863a2d5d000b9c0b02d76dfb5
tiagosathler <sathler@gmail.com>
2021-07-30 13:25:35 -0300

    Exercício 4: done

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercises.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercises.js
@@ -165,3 +165,35 @@ const fantasyOrScienceFiction = () => {
 };
 
 assert.deepStrictEqual(fantasyOrScienceFiction(), expectedResult);
+
+//4:
+expectedResult = [
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: { name: "H. P. Lovecraft", birthYear: 1890 },
+    releaseYear: 1928,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: { name: "Isaac Asimov", birthYear: 1920 },
+    releaseYear: 1951,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: { name: "J. R. R. Tolkien", birthYear: 1892 },
+    releaseYear: 1954,
+  },
+];
+
+const oldBooksOrdered = () =>
+  books
+    .filter((obj) => 2021 - obj.releaseYear > 60)
+    .sort((a, b) => a.releaseYear - b.releaseYear);
+
+assert.deepStrictEqual(oldBooksOrdered(), expectedResult);

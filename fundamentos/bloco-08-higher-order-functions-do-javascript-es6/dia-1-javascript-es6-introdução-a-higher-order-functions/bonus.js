683de9361f28dc03e464c89ba3613f7588e20ee8
tiagosathler <sathler@gmail.com>
2021-08-01 10:25:34 -0300

    Exercício Bonus: concluído

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introdução-a-higher-order-functions/bonus.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introdução-a-higher-order-functions/bonus.js
@@ -24,6 +24,9 @@ const dragon = {
 const battleMembers = { mage, warrior, dragon };
 
 // PARTE 1 - EXERCÍCIO 1:
+// Crie uma função que retorna o dano do dragão.
+// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
+
 const randomDamage = (min, max) => Math.round(Math.random()*(max-min))+min;
 
 const damageSimulator = (figure, a, b, callback) => {
@@ -38,48 +41,60 @@ const damageSimulator = (figure, a, b, callback) => {
   figure.damage = callback(a, b);
 }
 
-damageSimulator(dragon, 15, dragon.strength, randomDamage);
-console.log(`Dano do 'dragon': ${dragon.damage}`);
+// damageSimulator(dragon, 15, dragon.strength, randomDamage);
+// console.log(`Dano do 'dragon': ${dragon.damage}`);
 
 // PARTE 1 - EXERCÍCIO 2:
-// const warriorAttack = () => 
-damageSimulator(warrior, warrior.strength, warrior.strength*warrior.weaponDmg, randomDamage);
-console.log(`Dano do 'warrior': ${warrior.damage}`);
+// Crie uma função que retorna o dano causado pelo warrior .
+// O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+// damageSimulator(warrior, warrior.strength, warrior.strength*warrior.weaponDmg, randomDamage);
+// console.log(`Dano do 'warrior': ${warrior.damage}`);
 
 // PARTE 1 - EXERCÍCIO 3:
-const putMageDamageTurn = () => {
+// Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+const mageDamageTurn = () => {
   damageSimulator(mage, mage.intelligence, mage.intelligence*2, randomDamage); 
   return { damage: mage.damage, mana: mage.mana }
 }
-console.log(putMageDamageTurn());
+// console.log(mageDamageTurn());
 
 // PARTE 2:
-const gameActions = (attack, callback) => {
-  // Crie as HOFs neste objeto.
-  if (attack === warrior) {
-    // EXERCÍCIO 1:
-    callback(attack, attack.strength, attack.strength*attack.weaponDmg, randomDamage);
-    dragon.healthPoints -= attack.damage;
-  } else if (attack === mage) {    
-    callback(attack, attack.intelligence, attack.intelligence*2, randomDamage);
-    dragon.healthPoints -= attack.damage;
-  } else {
-    callback(attack, 15, attack.strength, randomDamage);
-    mage.healthPoints -= attack.damage;
-    warrior.healthPoints -= attack.damage;
-  }
-  return battleMembers;  
+const gameActions = {
+  warrior: (callback) => {
+    callback(warrior, warrior.strength, warrior.strength*warrior.weaponDmg, randomDamage);
+    dragon.healthPoints -= warrior.damage;
+  },
+  mage: (callback) =>  {
+    callback(mage, mage.intelligence, mage.intelligence*2, randomDamage);
+    if (typeof(mage.damage) === 'number') {
+      dragon.healthPoints -= mage.damage;
+    }    
+  },
+  dragon: (callback) => {
+    callback(dragon, 15, dragon.strength, randomDamage);
+    mage.healthPoints -= dragon.damage;
+    warrior.healthPoints -= dragon.damage;
+  },
+  fetchBattle: () => battleMembers,
 };
-console.log('1 Ataque do warrior:')
-console.log(gameActions(warrior, damageSimulator));
-console.log('1 Ataque do warrior:')
-console.log(gameActions(mage, damageSimulator));
-console.log('1 Ataque do dragon:')
-console.log(gameActions(dragon, damageSimulator));
 
-console.log('2 Ataque do warrior:')
-console.log(gameActions(warrior, damageSimulator));
-console.log('2 Ataque do warrior:')
-console.log(gameActions(mage, damageSimulator));
-console.log('2 Ataque do dragon:')
-console.log(gameActions(dragon, damageSimulator));
+const condition = () => warrior.healthPoints > 0 && mage.healthPoints > 0 && dragon.healthPoints > 0;
+
+const fullGame = () => {
+  let rodada = 0;
+  do {
+    console.log(`RODADA ${rodada}:`)
+    rodada += 1;
+    console.log(gameActions.fetchBattle());
+    gameActions.warrior(damageSimulator);
+    gameActions.mage(damageSimulator);
+    gameActions.dragon(damageSimulator);    
+  }
+  while (condition());
+  console.log(`FIM DE JOGO: finalizado na ${rodada}ª rodada!`);
+  console.log(gameActions.fetchBattle());  
+}
+
+fullGame();

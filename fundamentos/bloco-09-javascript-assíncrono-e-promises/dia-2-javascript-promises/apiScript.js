7315029f867fa9e78c762c0460ae2649b8f8e697
tiagosathler <sathler@gmail.com>
2021-08-11 21:36:33 -0300

    Exercício 02: concluído

--- fundamentos/bloco-09-javascript-assíncrono-e-promises/dia-2-javascript-promises/apiScript.js
+++ fundamentos/bloco-09-javascript-assíncrono-e-promises/dia-2-javascript-promises/apiScript.js
@@ -17,4 +17,29 @@ const fetchJoke = () => {
     .then((data) => pushJoke(data.joke));
 };
 
+const drawNumbers = new Promise((resolve, reject) => {
+  let array = [];
+  for (let i = 0; i < 10; i += 1) {
+    array.push(Math.ceil(Math.random() * 50));
+  }
+  array = array.map((number) => number * number);
+  const sum = array.reduce((acc, number) => number + acc);
+  if (sum >= 8000) {
+    return reject(sum);
+  }
+  resolve(sum);
+})
+  .then((number) => {
+    console.log(`Promise resolvida: ${number}`);
+    return [2, 3, 5, 10].map((num) => number / num);
+  })
+  .then((array) => {
+    console.log(array);
+    return array.reduce((acc, number) => acc + number);
+  })
+  .then((number) => console.log(`Soma do array: ${number}`))
+  .catch((number) => console.log(`É mais de oito mil (${number})! Esta promise deve estar quebrada`));
+  
+
+
 window.onload = () => fetchJoke();

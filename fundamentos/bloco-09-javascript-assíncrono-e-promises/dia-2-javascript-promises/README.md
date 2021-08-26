300f836c795bf425ea2e80dd2e0920c317a0afa0
tiagosathler <sathler@gmail.com>
2021-08-25 21:06:23 -0300

    Finalizado os exercícios do dia

---
+++ fundamentos/bloco-09-javascript-assíncrono-e-promises/dia-2-javascript-promises/README.md
@@ -0,0 +1,6 @@
+# Bloco 09 - Dia 2 - JavaScript Promises
+
+As **Promises**, embora não sejam muito intuitivas a princípio, foram criadas para facilitar a leitura de códigos assíncronos e tornar sua lógica mais previsível e legível. Elas são uma das formas mais populares de se escrever esse tipo de código em JavaScript hoje em dia. São também a principal forma de você se comunicar com APIs. A função do tipo *promise* é assíncrona, sendo literalmente uma promessa de entregar algum resultado. Saber como usá-la e tratar seu retorno é objetivo deste dia.
+
+## Exercícios do dia
+
--- fundamentos/bloco-09-javascript-assíncrono-e-promises/dia-2-javascript-promises/exercises-pre-aula.js
+++ fundamentos/bloco-09-javascript-assíncrono-e-promises/dia-2-javascript-promises/exercises-pre-aula.js
@@ -1,15 +1,3 @@
-// const promise = new Promise((resolve, reject) => {
-//   const number = Math.floor(Math.random()* 11);
-
-//   if (number <= 5) {
-//     return reject(number);
-//   }
-//   resolve(number);
-// })
-// .then(number => `Que sucesso =) nosso número foi ${number}`)
-// .then(msg => console.log(msg))
-// .catch(number => console.log(`Que fracasso =( Nosso número foi ${number}`));
-
 const fetch = require('node-fetch');
 
 async function verifiedFetch(url) {
@@ -29,11 +17,3 @@ async function sendJokeToFriend(name) {
 }
 
 sendJokeToFriend("Anna");
-// for(let i = 1; i < 5; i+=1) {
-//   setTimeout(() => {
-//     console.log(i);
-//   }, 1000);
-// }
-
-// const result = verifiedFetch('https://api.chucknorris.io/jokes/random?category=dev');
-// console.log(result);

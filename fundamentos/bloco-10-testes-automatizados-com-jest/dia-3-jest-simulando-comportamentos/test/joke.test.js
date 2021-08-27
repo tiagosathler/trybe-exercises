6d41b1db6b2d91a7c4aa5de7ad8d456101a91602
tiagosathler <sathler@gmail.com>
2021-08-27 16:35:03 -0300

    Exercício bônus: concluído

---
+++ fundamentos/bloco-10-testes-automatizados-com-jest/dia-3-jest-simulando-comportamentos/test/joke.test.js
@@ -0,0 +1,27 @@
+const { fetchJoke  } = require("../script/joke");
+const fetch = require("node-fetch");
+jest.mock("node-fetch");
+
+const someJoke = {
+  "id": "R7UfaahVfFd",
+  "joke": "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
+  "status": 200
+};
+
+describe("Mockando 'fetch' de 'fetchJoke'", () => {
+
+  // const fetch = jest.fn();
+  fetch.mockImplementation(() => 
+    Promise.resolve({
+      json: () => 
+        Promise.resolve(someJoke)
+    })
+  );
+
+  it("... testa se retorna o mock de 'fetch' na chamada de 'fetchJoke()'", () => {
+    expect(fetchJoke()).resolves.toEqual("My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.")    
+  })
+  it("... testa se a função 'fetch' foi realmente chamada", () => {
+    expect(fetch).toHaveBeenCalled();
+  });
+});

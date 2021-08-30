2465a2c27ed8095fb308fa982f1aaccce9d4f72c
tiagosathler <sathler@gmail.com>
2021-08-30 18:08:27 -0300

    Exercicio 2: concluido

--- front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/my-app/src/App.js
+++ front-end/bloco-11-introducao-a-react/dia-1-hello-world-no-react/my-app/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Task = (value) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>{value}</li>
+  )
+}
+
+const array = [
+  'Estudar inglês',
+  'Terminar o curso de oratória da Conquer',
+  'Programar a semana',
+];
+
+function App() {
+  
+  return (
+    <ol>
+      {Task('Estudar React')}
+      {array.map((element) => Task(element))}
+    </ol>
   );
 }
 

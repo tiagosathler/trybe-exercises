bf8f751fff2b28526f2d651ae723514afc849c32
tiagosathler <sathler@gmail.com>
2021-09-10 10:57:20 -0300

    Exercício 3: parcialmente concluído

--- front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/components/Forms.js
+++ front-end/bloco-12-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/my-form-2.0/src/components/Forms.js
@@ -9,6 +9,8 @@ import Housing from './Housing';
 import Resumo from './Resumo';
 import Cargo from './Cargo';
 import DescricaoCargo from './DescricaoCargo';
+import Submit from './Submit';
+import Results from './Results';
 
 class Forms extends React.Component {
   constructor(props) {
@@ -26,11 +28,13 @@ class Forms extends React.Component {
       role: '',
       mouseStatus: false,
       roleDescription: '',
+      submit: false,
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleBlur = this.handleBlur.bind(this);
     this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange({ target }) {
@@ -56,6 +60,30 @@ class Forms extends React.Component {
     }
   }
 
+  // handleSubmit() {
+  //   const {
+  //     fullName,
+  //     // email,
+  //     // cpf,
+  //     // address,
+  //     // city,
+  //     // state,
+  //     // housing,
+  //     // curriculum,
+  //     // role,
+  //     // mouseStatus,
+  //     // roleDescription
+  //   } = this.state;
+  //   return (
+  //     <div>
+  //       <p>
+  //         Nome:
+  //         { fullName }
+  //       </p>
+  //     </div>
+  //   );
+  // }
+
   render() {
     const {
       fullName,
@@ -68,80 +96,100 @@ class Forms extends React.Component {
       curriculum,
       role,
       mouseStatus,
-      roleDescription } = this.state;
+      roleDescription,
+      submit,
+    } = this.state;
 
     return (
-      <form className="my-form">
-        <fieldset>
-          <legend>Dados pessoais</legend>
-          <Nome
-            name="fullName"
-            value={ fullName }
-            eventListener={ this.handleChange }
+      <main>
+        <form
+          className="my-form"
+          onSubmit={ (event) => {
+            event.preventDefault();
+            this.setState({ submit: true });
+          } }
+        >
+          <fieldset>
+            <legend>Dados pessoais</legend>
+            <Nome
+              name="fullName"
+              value={ fullName }
+              eventListener={ this.handleChange }
+            />
+            <br />
+            <Email
+              name="email"
+              value={ email }
+              eventListener={ this.handleChange }
+            />
+            <br />
+            <Cpf
+              name="cpf"
+              value={ cpf }
+              eventListener={ this.handleChange }
+            />
+            <br />
+            <Endereco
+              name="address"
+              value={ address }
+              eventListener={ this.handleChange }
+            />
+            <br />
+            <Cidade
+              name="city"
+              value={ city }
+              eventListeners={ {
+                change: this.handleChange,
+                blur: this.handleBlur } }
+            />
+            <br />
+            <Estado
+              name="state"
+              value={ state }
+              eventListener={ this.handleChange }
+            />
+            <br />
+            <Housing
+              name="housing"
+              checked={ housing }
+              eventListener={ this.handleChange }
+            />
+          </fieldset>
+          <fieldset>
+            <legend>Dados sobre o último emprego</legend>
+            <Resumo
+              name="curriculum"
+              value={ curriculum }
+              eventListener={ this.handleChange }
+            />
+            <br />
+            <Cargo
+              name="role"
+              value={ role }
+              mouseStatus={ mouseStatus }
+              eventListeners={ {
+                change: this.handleChange,
+                mouseEnter: this.handleMouseEnter } }
+            />
+            <br />
+            <DescricaoCargo
+              name="roleDescription"
+              value={ roleDescription }
+              eventListener={ this.handleChange }
+            />
+            <Submit
+              name="submit"
+              eventListener={ this.handleSubmit }
+            />
+          </fieldset>
+        </form>
+        <section>
+          <Results
+            submit={ submit }
+            form={ this.state }
           />
-          <br />
-          <Email
-            name="email"
-            value={ email }
-            eventListener={ this.handleChange }
-          />
-          <br />
-          <Cpf
-            name="cpf"
-            value={ cpf }
-            eventListener={ this.handleChange }
-          />
-          <br />
-          <Endereco
-            name="address"
-            value={ address }
-            eventListener={ this.handleChange }
-          />
-          <br />
-          <Cidade
-            name="city"
-            value={ city }
-            eventListeners={ {
-              change: this.handleChange,
-              blur: this.handleBlur } }
-          />
-          <br />
-          <Estado
-            name="state"
-            value={ state }
-            eventListener={ this.handleChange }
-          />
-          <br />
-          <Housing
-            name="housing"
-            checked={ housing }
-            eventListener={ this.handleChange }
-          />
-        </fieldset>
-        <fieldset>
-          <legend>Dados sobre o último emprego</legend>
-          <Resumo
-            name="curriculum"
-            value={ curriculum }
-            eventListener={ this.handleChange }
-          />
-          <br />
-          <Cargo
-            name="role"
-            value={ role }
-            mouseStatus={ mouseStatus }
-            eventListeners={ {
-              change: this.handleChange,
-              mouseEnter: this.handleMouseEnter } }
-          />
-          <br />
-          <DescricaoCargo
-            name="roleDescription"
-            value={ roleDescription }
-            eventListener={ this.handleChange }
-          />
-        </fieldset>
-      </form>
+        </section>
+      </main>
     );
   }
 }

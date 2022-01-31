9987c034e2f76b52043e8747d977c2d6a4295bec
tiagosathler <sathler@gmail.com>
2022-01-31 15:28:27 -0300

    Bloco 22 - Dia 1 - Exercícios pos-aula: 4-5

--- back-end/bloco-22/dia-1/pos-aula/index.js
+++ back-end/bloco-22/dia-1/pos-aula/index.js
@@ -1,9 +1,16 @@
 const read = require('readline-sync');
-const { imc } = require('./services/imc');
+const { imc, compare } = require('./services/imc');
+
 
 const nome = read.question('Qual seu nome?');
-const peso = read.questionInt('Qual seu peso (em kg)?');
-const altura = read.questionInt('Qual sua altura (em m)?');
+const peso = read.questionFloat('Qual seu peso (em kg)?');
+const altura = read.questionFloat('Qual sua altura (em m)?');
 
 const yourImc = imc(peso, altura);
-console.log(`Olá ${nome}, seu IMC é de ${yourImc}`);
+console.log(`Olá ${nome}! Peso: ${peso} kg / Altura: ${altura} m`);
+console.log(`Seu IMC é de ${yourImc}`);
+
+const { range, message } = compare(yourImc);
+
+console.log(`Sua faixa de IMC: ${range}`);
+console.log(`Sua condição: ${message}`);

d06d949e3e636648944c03837d130a632cf2eff8
tiagosathler <sathler@gmail.com>
2022-02-01 19:17:34 -0300

    Bloco 22 - Dia 2 - Exercises 1-4 done

--- back-end/bloco-22/dia-2/index.js
+++ back-end/bloco-22/dia-2/index.js
@@ -1,17 +1,19 @@
+const fs = require('fs').promises;
+
 // 1. Crie uma função que recebe três parâmetros retorna uma Promise .
 async function one(...numbers) {
   return new Promise((resolve, reject) => {
     console.log(numbers);
     if (numbers.length !== 3) reject(new Error('Informe 3 números'));
     numbers.forEach((number) => {
-      if (Number.isNaN(Number(number))) {
+      if (typeof number !== 'number') {
         reject(new Error('Informe apenas números'));
       }      
     })
     const [a, b, c] = numbers;
     const result = (a + b) * c;
-    if (result < 5000) reject(new Error('Valor muito baixo'));
-    resolve(result);
+    if (result < 5000) reject({result, message: 'Resultado abaixo de 5000!'});
+    resolve({result, message: 'Resultado acima de 5000!'});
   })
 }
 
@@ -37,8 +39,8 @@ console.log('------------------------------')
 function two() {
   const random = ['','',''].map(() => Math.floor(Math.random() * 100 + 1));
   one(...random)
-    .then((result) => console.log('2: ', result))
-    .catch((error) => console.log('2: ', error));
+    .then((response) => console.log(`2: SUCCESS - ${response.message} - result = `, response.result))
+    .catch((response) => console.log(`2: FAILS - ${response.message} - result = `, response.result));
 }
 
 two();
@@ -48,10 +50,10 @@ console.log('------------------------------')
 async function three() {
   try {
     const random = ['','',''].map(() => Math.floor(Math.random() * 100 + 1));
-    const result = await one(...random);
-    console.log('3: ', result)
-  } catch(error) {
-    console.error('3: ', error);
+    const resolve = await one(...random);
+    console.log(`3: SUCCESS - ${resolve.message} - result = `, resolve.result);
+  } catch(reject) {
+    console.log(`3: FAILS - ${reject.message} - result = `, reject.result);
   }
 }
 
@@ -59,15 +61,15 @@ three();
 
 // 4. Realize o download deste arquivo e salve-o como simpsons.json . Utilize o arquivo baixado para realizar os requisitos abaixo.
 console.log('------------------------------')
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 
 function part41(content) {
   content.forEach(( {id, name }) => console.log(`${id} - ${name}`));
 }
 
-function part42(content)
+// function part42(content)
 
-fs.readFile('./simpsons.json', {encoding: 'utf-8', flag: 'r' })
+fs.readFile('./simpsons.json', 'utf-8')
   .then((json) => JSON.parse(json))
   .then((content) => {
     part41(content);

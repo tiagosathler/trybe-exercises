bb3931e577a5acc3b1ab8fdd20b43159eaedc48b
tiagosathler <sathler@gmail.com>
2022-02-20 20:52:56 -0300

    Bloco 24 - Dia 1 - ORM - Pos-aula - Books Example - Concluido

--- back-end/bloco-24/dia-1/pos-aula/books/src/controller/index.js
+++ back-end/bloco-24/dia-1/pos-aula/books/src/controller/index.js
@@ -12,6 +12,68 @@ BooksRoute
       console.log(e.message);
       return res.status(500).json({ message: e.message });
     }
+  })
+  .get('/:id', async (req, res) => {
+    try {
+      const { params: { id } } = req;
+      const books = await Book.findByPk(id);
+      if (books === null) return res.status(404).json({ message: 'Livro não encontrado' });
+      return res.status(200).json(books);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).json({ message: e.message });
+    }
+  })
+  .get('/search/:id', async (req, res) => {
+    try {
+      const { query: { title }, params: { id } } = req;
+      console.log(title);
+      const books = await Book.findOne({ where: { id, title } });
+      if (books === null) return res.status(404).json({ message: 'Livro não encontrado' });
+
+      return res.status(200).json(books);
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).json({ message: e.message });
+    }
+  })
+  .post('/', async (req, res) => {
+    try {
+      const { body: { title, author, pageQuantity } } = req;
+      const created = await Book.create({ title, author, pageQuantity });
+      // if (response === 0) return res.status(406).json({ message: 'Não adcionado' });
+
+      return res.status(201).json({ created });
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).json({ message: e.message });
+    }
+  })
+  .put('/:id', async (req, res) => {
+    try {
+      const { body: { title, author, pageQuantity }, params: { id } } = req;
+      const response = await Book
+        .update({ title, author, pageQuantity }, { where: { id } });
+      if (response === 0) return res.status(406).json({ message: 'Não modificado' });
+
+      return res.status(202).json({ message: 'Atualizado com sucesso' });
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).json({ message: e.message });
+    }
+  })
+  .delete('/:id', async (req, res) => {
+    try {
+      const { params: { id } } = req;
+      const response = await Book
+        .destroy({ where: { id } });
+      if (response === 0) return res.status(406).json({ message: 'Não apagado' });
+
+      return res.status(202).json({ message: 'Apagado com sucesso' });
+    } catch (e) {
+      console.log(e.message);
+      return res.status(500).json({ message: e.message });
+    }
   });
 
 module.exports = BooksRoute;

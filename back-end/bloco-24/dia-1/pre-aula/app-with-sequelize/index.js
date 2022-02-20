6dd818c7951b2b36ff99fe4da22fd359a70a92a5
tiagosathler <sathler@gmail.com>
2022-02-20 13:36:17 -0300

    Bloco 24 - Dia 1 - ORM - Criado o 'UserController' com 'Router' e criado o 'index' principal da API

---
+++ back-end/bloco-24/dia-1/pre-aula/app-with-sequelize/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const UserRoute = require('./controllers/userController');
+
+const app = express();
+
+app.use(bodyParser.json(), cors());
+
+app.use('/users', UserRoute);
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`App online listening at PORT ${PORT}`));

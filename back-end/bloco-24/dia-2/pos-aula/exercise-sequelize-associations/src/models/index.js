fb20113807141a094323615df1d807eba82b8b41
tiagosathler <sathler@gmail.com>
2022-03-08 20:44:45 -0300

    Bloco 24 - Dia 2 - pos-aula - exercícios - (refatoração)

--- back-end/bloco-24/dia-2/pos-aula/exercise-sequelize-associations/src/models/index.js
+++ back-end/bloco-24/dia-2/pos-aula/exercise-sequelize-associations/src/models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV === 'test' ? 'test' : 'development';
 const config = require(`${__dirname}/../config/config.js`)[env];
-console.log(config.use_env_variable);
+
 const db = {};
 
 let sequelize;

f12274eaa8e8c0719bc6721246f30bc65706cef2
tiagosathler <sathler@gmail.com>
2022-04-01 21:56:52 -0300

    Bloco 27 - Dia 1 - Pós aula - Snack Bar management

--- back-end/bloco-27/dia-1/pos-aula/snackbar/src/class/order.ts
+++ back-end/bloco-27/dia-1/pos-aula/snackbar/src/class/order.ts
@@ -74,15 +74,18 @@ export default class Order {
 
   public removeItem(item: OrderItem): void {
     const { product } = item;
-    const filtered = this._items.filter((i) => i.product.toLowerCase() !== product.toLowerCase());
-    this._items = filtered;
+    console.log('PRODUCT', product);
+    const filtered: OrderItem[] = this._items
+      .filter((i) => i.product.toLowerCase() !== product.toLowerCase());
+    this._items = [...filtered];
   }
 
   public calculateOrder(): number {
     const total = this._items.reduce((acc, item) => acc + item.price, 0);
     if (this._discount > 0) {
-      return total * (1 - this._discount);
+      const value = total * (1 - this._discount);
+      return Number(value.toFixed(2));
     }
-    return total;    
+    return Number(total.toFixed(2));
   }
 }
--- back-end/bloco-27/dia-1/pos-aula/snackbar/src/index.ts
+++ back-end/bloco-27/dia-1/pos-aula/snackbar/src/index.ts
@@ -9,35 +9,43 @@ const orderItem1 = new OrderItem('Pão de queijo', 3);
 const orderItem2 = new OrderItem('Misto quente', 7);
 const orderItem3 = new OrderItem('Café expreso', 5);
 const orderItem4 = new OrderItem('Café com leite', 4);
+const orderItem5 = new OrderItem('Coca-cola', 3);
 
+console.log('=====> Order 1:');
 const order1 = new Order(client1, [orderItem1, orderItem3], 'Dinheiro');
+
+console.log('Client [1]: ', order1.client.name);
+console.log('Items [1]: ', order1.items);
+console.log('Payment method [1]: ', order1.paymentMethod);
+console.log('Discount [1]: ', order1.discount);
+console.log('Total [1]: ', order1.calculateOrder());
+
+order1.addItem(orderItem5);
+order1.removeItem(orderItem3);
+order1.paymentMethod = 'Cartão de débito';
+
+console.log('Items [1]: ', order1.items);
+console.log('Payment method [1]: ', order1.paymentMethod);
+console.log('Discount [1]: ', order1.discount);
+console.log('Total [1]: ', order1.calculateOrder());
+
+console.log('=====> Order 2:');
 const order2 = new Order(client2, [orderItem2, orderItem4], 'Cartão');
 
-const closedOrder1 = order1.calculateOrder();
-const closedOrder2 = order2.calculateOrder();
-
-console.log('--- Order 1:');
-console.log('Client: ', order1.client.name);
-console.log('Items: ', order1.items);
-console.log('Payment method: ', order1.paymentMethod);
-console.log('Discount: ', order1.discount);
-console.log('Total: ', closedOrder1);
-
-console.log('--- Order 2:');
-console.log('Client: ', order2.client.name);
-console.log('Items: ', order2.items);
-console.log('Payment method: ', order2.paymentMethod);
-console.log('Discount: ', order1.discount);
-console.log('Total: ', closedOrder2);
+console.log('Clien [2]: ', order2.client.name);
+console.log('Items [2]: ', order2.items);
+console.log('Payment method [2]: ', order2.paymentMethod);
+console.log('Discount [2]: ', order1.discount);
+console.log('Total [2]: ', order2.calculateOrder());
 
 console.log('===> crying');
 order1.discount = 0.3;
 order2.discount = 0.5;
 
-console.log('--- Order 1:');
+console.log('=====> Order 1:');
 console.log('Discount: ', order1.discount);
 console.log('Total: ', order1.calculateOrder());
 
-console.log('--- Order 2:');
+console.log('=====> Order 2:');
 console.log('Discount: ', order2.discount);
 console.log('Total: ', order2.calculateOrder());

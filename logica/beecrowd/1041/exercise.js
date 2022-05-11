302ec975ba37783ed8a82a8ce051d9c60e84194d
Tiago <sathler@gmail.com>
2022-05-11 11:53:13 -0300

    Beecrowd 1041 done

---
+++ logica/beecrowd/1041/exercise.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('./dev/stdin', 'utf8');
+const [x, y] = input.trim().split(' ');
+
+if (+x > 0 && +y > 0) {
+  console.log('Q1');
+} else if (+x < 0 && +y > 0) {
+  console.log('Q2');
+} else if (+x < 0 && +y < 0) {
+  console.log('Q3');
+} else if (+x > 0 && +y < 0) {
+  console.log('Q4');
+} else if (+x === 0 && +y === 0) {
+  console.log('Origem');
+} else if (+x === 0) {
+  console.log('Eixo Y');
+} else {
+  console.log('Eixo X');
+}

3558f40279dc333c0b0d507464d10e7e2b1c23da
Tiago <sathler@gmail.com>
2022-05-24 16:50:56 -0300

    feat: change arrow fucntion to function in abstract model

--- back-end/bloco-30/dia-2/pre-aula/glasses/src/models/MongoModel.ts
+++ back-end/bloco-30/dia-2/pre-aula/glasses/src/models/MongoModel.ts
@@ -4,12 +4,17 @@ import Model from '.';
 abstract class MongoModel<T> implements Model<T> {
   constructor(protected model: M<T & Document>) { }
 
-  create = async (obj: T): Promise<T> => this.model.create({ ...obj });
+  public async create(obj: T): Promise<T> { 
+    return this.model.create({ ...obj });
+  }
 
-  read = async (): Promise<T[]> => this.model.find();
+  public async read(): Promise<T[]> {
+    return this.model.find();
+  }
 
-  readOne = async (id: string): Promise<T | null> =>
-    this.model.findOne({ _id: id });
+  public async readOne(id: string): Promise<T | null> {
+    return this.model.findOne({ _id: id });
+  }
 }
 
 export default MongoModel;

2cb78fa7fa2e762dbe5c9c84c5b6c53ab2927500
Tiago <sathler@gmail.com>
2022-05-24 13:58:06 -0300

    fix: change Frame.ts interface

--- back-end/bloco-30/dia-2/pre-aula/glasses/src/interfaces/Frame.ts
+++ back-end/bloco-30/dia-2/pre-aula/glasses/src/interfaces/Frame.ts
@@ -1,6 +1,14 @@
-interface Frame {
-  material: string,
-  color: string,
-}
+import { z } from 'zod';
+
+const FrameSchema = z.object({
+  material: z.string(),
+  color: z.string({
+    required_error: 'Color is required',
+    invalid_type_error: 'Color must be a string',
+  }).min(3, { message: 'Color must be 3 or more characters long' }),
+});
+
+type Frame = z.infer<typeof FrameSchema>;
 
 export default Frame;
+export { FrameSchema };

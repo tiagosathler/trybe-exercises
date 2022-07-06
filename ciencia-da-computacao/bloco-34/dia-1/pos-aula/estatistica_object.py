e450120f220471e4f6c7965019e0a2b1b2f4b602
Tiago <sathler@gmail.com>
2022-07-06 16:26:33 -0300

    :sparkles: feat[figuras]: create abstract class Figuras; create Quadrado, Reatangulo and Circulo classes; figuras_object program

--- ciencia-da-computacao/bloco-34/dia-1/pos-aula/estatistica_object.py
+++ ciencia-da-computacao/bloco-34/dia-1/pos-aula/estatistica_object.py
@@ -1,12 +1,12 @@
 from src.estatistica import Estatistica
 
-my_list = Estatistica([0, 1, 2, 3, 4, 5, 6])
+my_list = Estatistica([0, 1, 2, 3, 4, 5, 6, 6])
 print(f"média: {my_list.media()}")
 print(f"mediana: {my_list.mediana()}")
 print(f"moda: {my_list.moda()}")
 
 
-my_list = Estatistica([5, 7, 12, -3, 5, 4, 45, 600])
+my_list = Estatistica([5, 7, 12, -3, 5, 4.5, 45, 600])
 print(f"média: {my_list.media()}")
 print(f"mediana: {my_list.mediana()}")
 print(f"moda: {my_list.moda()}")

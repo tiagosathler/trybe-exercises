bae5bb86da6309eebc79d0edb80a64b14c06ea87
Tiago <sathler@gmail.com>
2022-08-03 12:16:24 -0300

    :recycle: refactor: refatora o exercício 2

--- ciencia-da-computacao/bloco-37/dia-2/pos-aula/challenges/src/exercise_2.py
+++ ciencia-da-computacao/bloco-37/dia-2/pos-aula/challenges/src/exercise_2.py
@@ -5,35 +5,44 @@ def shuffle(numbers: list[int], n: int) -> list[int]:
     Faremos isto dividindo uma porção de cartas em 2 e depois mesclando
     as duas porções.
 
-    Complexidade:
+    Complexidade: O(n)
 
     Entrada:
     --------
 
-    arr: list[int]
-       cartas
+    numbers: list[int]
+       cada número das cartas
 
-    grupo: int
-        grupo de cartas para embaralhar
+    n: int
+        n cartas para embaralhar
 
     Saída:
     ------
 
-    result -> list[int]
-        cartas embaralhadas
+    result:
+        list[int]
+            cartas embaralhadas
+        None
+            quando o tamanho da lista de cartas for ímpar
     """
 
-    # SOLUÇÃO DO GABARITO:
+    # SOLUÇÃO DO GABARITO (refatorado):
     answer = []
     new_array_index = 0
+
+    if (len(numbers) % 2):
+        return None
+
     for index in range(n):
         answer.insert(new_array_index, numbers[index])
         new_array_index += 1
         answer.insert(new_array_index, numbers[index + n])
         new_array_index += 1
+
     return answer
 
 
 def test_shuffle():
     assert shuffle([2, 6, 4, 5], 2) == [2, 4, 6, 5]
     assert shuffle([1, 4, 4, 7, 6, 6], 3) == [1, 7, 4, 6, 4, 6]
+    assert shuffle([1, 4, 4, 5, 7, 6, 6], 3) is None

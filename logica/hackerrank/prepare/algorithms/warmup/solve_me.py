f3edafa4ae961d12d4338090634a4f927adf3736
Tiago <sathler@gmail.com>
2022-09-02 10:07:05 -0300

    :sparkles: feat: conclui o desafio 'Solve me'

---
+++ logica/hackerrank/prepare/algorithms/warmup/solve_me.py
@@ -0,0 +1,30 @@
+def solveMeFirst(a: int, b: int) -> int:
+    """
+    https://www.hackerrank.com/challenges/solve-me-first/problem?isFullScreen=true
+
+    Complete the function solveMeFirst to compute the sum of two integers.
+
+    Example
+    a = 7
+    b = 3
+    Return 10.
+
+    Function Description
+
+    Complete the solveMeFirst function in the editor below.
+
+    solveMeFirst has the following parameters:
+
+    int a: the first value
+    int b: the second value
+    Returns
+    - int: the sum of a and b
+    """
+    return a + b
+
+
+if __name__ == "__main__":
+    num1 = int(input())
+    num2 = int(input())
+    res = solveMeFirst(num1, num2)
+    print(res)

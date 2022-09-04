e60c21e5cffb445019b03b66aa84ce3b8ae3dde3
Tiago <sathler@gmail.com>
2022-09-04 11:45:45 -0300

    :sparkles: feat: conclui o desafio 'Min Max Sum'

---
+++ logica/hackerrank/prepare/algorithms/warmup/src/min_max_sum.py
@@ -0,0 +1,83 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import math
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    """
+    https://www.hackerrank.com/challenges/mini-max-sum
+    Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+    Example
+
+    The minimum sum is  and the maximum sum is . The function prints
+
+    16 24
+    Function Description
+
+    Complete the miniMaxSum function in the editor below.
+
+    miniMaxSum has the following parameter(s):
+
+    arr: an array of  integers
+    Print
+
+    Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.
+
+    Input Format
+
+    A single line of five space-separated integers.
+
+    Constraints
+
+
+    Output Format
+
+    Print two space-separated long integers denoting the respective minimum and maximum values that can be calculated by summing exactly four of the five integers. (The output can be greater than a 32 bit integer.)
+
+    Sample Input
+
+    1 2 3 4 5
+    Sample Output
+
+    10 14
+    Explanation
+
+    The numbers are , , , , and . Calculate the following sums using four of the five integers:
+
+    Sum everything except , the sum is .
+    Sum everything except , the sum is .
+    Sum everything except , the sum is .
+    Sum everything except , the sum is .
+    Sum everything except , the sum is .
+    """
+    min = math.inf
+    max = -math.inf
+
+    for i in range(len(arr)):
+        sum = 0
+        for j, y in enumerate(arr):
+            if (i != j):
+                sum += y
+        if sum < min:
+            min = sum
+        if sum > max:
+            max = sum
+
+    print("%d %d" % (min, max))
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)

7a5175cb8eadd5bf10e8314e4335e3c2ffadebc4
Tiago <sathler@gmail.com>
2022-09-08 16:38:37 -0300

    :sparkles: feat: conclui o desafio 'Migratory Birds'

---
+++ logica/hackerrank/prepare/algorithms/warmup/src/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+for id, count in birds.items():
+    #     if count > count_ref:
+    #         id_ref = id
+    #         count_ref = count
+    #     elif count == count_ref and id < id_ref:
+    #         id_ref = id
+    #         count_ref = count
